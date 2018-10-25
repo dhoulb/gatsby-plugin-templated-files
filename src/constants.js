c726a7c487d1fa86e6a6937dedd04a5a6405ea4e
Dave Houlbrooke <dave@shax.com>
2018-10-25 19:15:28 +0100

    fix: cleaner default file ignore list

--- src/constants.js
+++ src/constants.js
@@ -2,15 +2,11 @@ exports.TYPE = "Templated";
 exports.URL = "/:slug";
 exports.INCLUDE = ["**/*.md"];
 exports.IGNORE = [
-	"**/*.un~",
-	"**/.DS_Store",
-	"**/.gitignore",
-	"**/.npmignore",
-	"**/.babelrc",
+	"**/.*",
 	"**/yarn.lock",
-	"**/bower_components",
-	"**/node_modules",
-	"../**/dist/**"
+	"**/package.json",
+	"**/package-lock.json",
+	"**/node_modules"
 ];
 exports.INDEXES = ["**/index.*", "**/README.*"];
 exports.TEMPLATES = "src/templates";
